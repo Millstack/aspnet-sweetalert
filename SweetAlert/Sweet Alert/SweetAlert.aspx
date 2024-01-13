5d55f285085ace12ecd52b38bf494bb792043aae
Milind Khamkar <fullstackwithmilind@gmail.com>
2024-01-13 17:10:30 +0530

    sweet alert different sets

---
+++ SweetAlert/Sweet Alert/SweetAlert.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SweetAlert.aspx.cs" Inherits="Sweet_Alert_SweetAlert" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Sweet Alert</title>
+
+    <!--Bootstrap CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <!--Bootstrap JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!--jQuery-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+
+    <!-- SweetAlert2 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.3/dist/sweetalert2.min.css" rel="stylesheet" />
+    <!-- SweetAlert2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.3/dist/sweetalert2.all.min.js"></script>
+    <!-- https://sweetalert2.github.io/# -->
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="card col-md-6 mx-auto mt-5 py-2 border border-secondary-subtle rounded-2 shadow">
+            <label class="text-center">Sweet Alert</label>
+            <div class="card-body text-center">
+                <asp:Button ID="btnAlert" runat="server" Text="JavaScript Alert" OnClick="btnAlert_Click" CssClass="btn btn-secondary col-md-5" />
+            </div>
+            <div class="card-body text-center">
+                <asp:Button ID="btnQuestion" runat="server" Text="Question ?" OnClick="btnQuestion_Click" CssClass="btn btn-white border border-secondary-subtle col-md-5" />
+            </div>
+            <div class="card-body text-center">
+                <asp:Button ID="btnInfo" runat="server" Text="Information !" OnClick="btnInfo_Click" CssClass="btn btn-info col-md-5" />
+            </div>
+            <div class="card-body text-center">
+                <asp:Button ID="btnSuccess" runat="server" Text="Success only" OnClick="btnSuccess_Click" CssClass="btn btn-success col-md-5" />
+            </div>
+            <div class="card-body text-center">
+                <asp:Button ID="btnError" runat="server" Text="Error only" OnClick="btnError_Click" CssClass="btn btn-danger col-md-5" />
+            </div>
+        </div>
+
+        <div class="card col-md-6 mx-auto mt-2 py-2 border border-secondary-subtle rounded-2 shadow">
+            <label class="text-center">Sweet Alert Advance</label>
+            <div class="card-body text-center">
+                <asp:Button ID="btn1" runat="server" Text="HTML, Icons, Links" OnClick="btn1_Click" CssClass="btn btn-success col-md-5" />
+            </div>
+            <div class="card-body text-center">
+                <asp:Button ID="Button1" runat="server" Text="3 Button Dialouge" OnClick="Button1_Click" CssClass="btn btn-warning col-md-5" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SweetAlert/Sweet Alert/SweetAlert.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Sweet_Alert_SweetAlert : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+        }
+    }
+
+    // javascript alert
+    protected void btnAlert_Click(object sender, EventArgs e)
+    {
+        ScriptManager.RegisterStartupScript(this, GetType(), "alertMessage", "alert('JavaScript Alert!');", true);
+    }
+
+    // sweet alert question
+    protected void btnQuestion_Click(object sender, EventArgs e)
+    {
+        string title = "No data found !";
+        string message = "Please check your query";
+        string icon = "question";
+        string confirmButtonText = "OK";
+
+        string sweetAlertScript = $"<script>Swal.fire({{ title: '{title}', text: '{message}', icon: '{icon}', confirmButtonText: '{confirmButtonText}' }});</script>";
+        ClientScript.RegisterStartupScript(this.GetType(), "sweetAlert", sweetAlertScript, false);
+    }
+
+    // sweet info question
+    protected void btnInfo_Click(object sender, EventArgs e)
+    {
+        string title = "Matched!";
+        string message = "Selected data matched";
+        string icon = "info";
+        string confirmButtonText = "OK";
+
+        string sweetAlertScript = $"<script>Swal.fire({{ title: '{title}', text: '{message}', icon: '{icon}', confirmButtonText: '{confirmButtonText}' }});</script>";
+        ClientScript.RegisterStartupScript(this.GetType(), "sweetAlert", sweetAlertScript, false);
+    }
+
+    // sweet alert success only
+    protected void btnSuccess_Click(object sender, EventArgs e)
+    {
+        string title = "Success!";
+        string message = "Saved successfully!";
+        string icon = "success";
+        string confirmButtonText = "OK";
+
+        string sweetAlertScript = $"<script>Swal.fire({{ title: '{title}', text: '{message}', icon: '{icon}', confirmButtonText: '{confirmButtonText}' }});</script>";
+        ClientScript.RegisterStartupScript(this.GetType(), "sweetAlert", sweetAlertScript, false);
+    }
+
+    // sweet alert error only
+    protected void btnError_Click(object sender, EventArgs e)
+    {
+        string title = "Oops!";
+        string message = "Please check again!";
+        string icon = "error";
+        string confirmButtonText = "OK";
+
+        string sweetAlertScript = $"<script>Swal.fire({{ title: '{title}', text: '{message}', icon: '{icon}', confirmButtonText: '{confirmButtonText}' }});</script>";
+        ClientScript.RegisterStartupScript(this.GetType(), "sweetAlert", sweetAlertScript, false);
+    }
+
+    //------------------============{ Sweet Alert More }============------------------
+
+    // sweet alert custom html, button and link
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        string title = "HTML example";
+        string icon = "info";
+        string htmlContent = @"You can use <b>bold text</b>,<a href='#'>links</a>,and other HTML tags";
+        bool showCloseButton = true;
+        bool showCancelButton = true;
+        bool focusConfirm = false;
+        string confirmButtonText = "<i class='fa fa-thumbs-up'></i> Great!";
+        string confirmButtonAriaLabel = "Thumbs up, great!";
+        string cancelButtonText = "<i class='fa fa-thumbs-down'></i>";
+        string cancelButtonAriaLabel = "Thumbs down";
+
+        string sweetAlertScript = $@"
+        <script>
+            Swal.fire({{
+                title: '{title}',
+                icon: '{icon}',
+                html: `{htmlContent}`,
+                showCloseButton: {showCloseButton.ToString().ToLower()},
+                showCancelButton: {showCancelButton.ToString().ToLower()},
+                focusConfirm: {focusConfirm.ToString().ToLower()},
+                confirmButtonText: `{confirmButtonText}`,
+                confirmButtonAriaLabel: '{confirmButtonAriaLabel}',
+                cancelButtonText: `{cancelButtonText}`,
+                cancelButtonAriaLabel: '{cancelButtonAriaLabel}'
+            }});
+        </script>";
+        ClientScript.RegisterStartupScript(this.GetType(), "sweetAlert", sweetAlertScript, false);
+
+    }
+
+    // sweet alert dialouge with three buttons
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string title = "Do you want to save the changes?";
+        bool showDenyButton = true;
+        bool showCancelButton = true;
+        string confirmButtonText = "Save";
+        string denyButtonText = "Dont Save";
+
+        string sweetAlertScript = $@"
+        <script>
+            Swal.fire({{
+                title: '{title}',
+                showDenyButton: {showDenyButton.ToString().ToLower()},
+                showCancelButton: {showCancelButton.ToString().ToLower()},
+                confirmButtonText: '{confirmButtonText}',
+                denyButtonText: '{denyButtonText}'
+            }}).then((result) => {{
+                if (result.isConfirmed) {{
+                    Swal.fire('Saved!', 'mssg', 'success');
+                }} else if (result.isDenied) {{
+                    Swal.fire('Changes are not saved', 'mssg', 'info');
+                }}
+            }});
+        </script>";
+
+        ClientScript.RegisterStartupScript(this.GetType(), "sweetAlert", sweetAlertScript, false);
+
+    }
+}
